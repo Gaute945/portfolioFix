0675535913b07e9b6ca470a32c9bbed305e926a5
Gaute <mrboobai@proton.me>
2023-12-14 16:32:51 +0100

    added profile and formated dates

--- js/githubAPI.js
+++ js/githubAPI.js
@@ -1,5 +1,22 @@
+function formatDate(dateString) {
+	// Create a new Date object from the given dateString
+	const date = new Date(dateString);
+
+	// Extract the individual components from the date object
+	const day = date.getDate();
+	const month = date.getMonth() + 1; // Adding 1 because months are zero-based
+	const year = date.getFullYear();
+
+	// Format the components into the desired format
+	const formattedDate = `${day}.${month}.${year}`;
+
+	return formattedDate;
+}
+
 async function repoData(repo, owner, num) {
-	const data = await fetch("https://api.github.com/repos/" + owner + "/" + repo);
+	const data = await fetch(
+		"https://api.github.com/repos/" + owner + "/" + repo
+	);
 	const json = await data.json();
 
 	name = json.name;
@@ -7,17 +24,58 @@ async function repoData(repo, owner, num) {
 	description = json.description;
 
 	language = json.language;
-    
+
 	license = json.license.key;
 
 	updated = json.updated_at;
+	const formattedDate = formatDate(updated); // Assign the result to formattedDate
 
-	document.getElementById("name" + num).innerText = name;
-	document.getElementById("description" + num).innerText = description;
-    document.getElementById("language" + num).innerText = language;
-	document.getElementById("license" + num).innerText = license;
-	document.getElementById("updated_at" + num).innerText = updated;
+	document.getElementById("name" + num).innerText = "name; " + name;
+	document.getElementById("description" + num).innerText =
+		"description; " + description;
+	document.getElementById("language" + num).innerText = "language; " + language;
+	document.getElementById("license" + num).innerText = "license; " + license;
+	document.getElementById("updated_at" + num).innerText =
+		"updated_at; " + formattedDate;
 }
+
+async function userData(user, num) {
+	const data = await fetch("https://api.github.com/users" + "/" + user);
+	const json = await data.json();
+
+	avatar_url = json.avatar_url;
+
+	name = json.name;
+
+	login = json.login;
+
+	bio = json.bio;
+
+	followers = json.followers;
+
+	following = json.following;
+
+	userLocation = json.location;
+
+	blog = json.blog;
+
+	const imageElement = document.getElementById("avatar_url" + num);
+	imageElement.src = avatar_url;
+	document.getElementById("name" + num).innerText = "name; " + name;
+	document.getElementById("login" + num).innerText = "login; " + login;
+	document.getElementById("bio" + num).innerText = "bio; " + bio;
+	document.getElementById("followers" + num).innerText =
+		"followers; " + followers;
+	document.getElementById("following" + num).innerText =
+		"following; " + following;
+	document.getElementById("location" + num).innerText =
+		"location; " + userLocation;
+	const linkElement = document.getElementById("blog" + num);
+	linkElement.href = blog;
+	document.getElementById("blog" + num).innerText = "blog; " + blog;
+}
+userData("Gaute945", 0);
+
 repoData("Overmounting", "Gaute945", 0);
 repoData("UserIsland", "Gaute945", 1);
 repoData("ParkingSensor", "Gaute945", 2);
