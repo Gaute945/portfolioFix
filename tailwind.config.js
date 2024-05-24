0cd63a2011084a48fb37de9e52d179a5c5a4de4d
Gaute <mrboobai@proton.me>
2024-05-24 19:37:31 +0200

    style amd logos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./views/**/*.ejs", "./public/**/*.css"],
   theme: {
-    extend: {},
+    fontFamily: {
+      brush: ['Brush Script', 'cursive'],
+    },
+    extend: {
+      colors: {
+        'very-dark-blue': '#2C0093',
+        'magenta': '#E100EB',
+        'violet': '#9500ea',
+        'blue': '#0004EB',
+        'pink': '#EB006F',
+        'soft-violet': '#B14EEB',
+      },
+    },
   },
   plugins: [],
 }
